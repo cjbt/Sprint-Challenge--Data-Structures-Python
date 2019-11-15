71cb177f9960625ee6e3c2e5832ff5b5a01716e3
cecil john tantay <cjbtantay@gmail.com>
2019-11-15 09:19:42 -0800

    finished names

--- names/names.py
+++ names/names.py
@@ -1,3 +1,4 @@
+from lru_cache import *
 import time
 
 start_time = time.time()
@@ -11,10 +12,21 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+            # duplicates.append(name_1)
+cache = LRUCache(10000)
+for name in names_1:
+    cache.set(name, name)
+for name in names_2:
+    if name == cache.get(name):
+        duplicates.append(name)
+
+
+"""
+find duplicates
+"""
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
