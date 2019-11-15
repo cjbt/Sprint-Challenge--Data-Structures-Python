efa8f770a3e945044c54781b5c2e7971fc5528dc
cecil john tantay <cjbtantay@gmail.com>
2019-11-15 09:30:19 -0800

    finish ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,29 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
-
+    # if self.current < self.capacity:
+    if self.current == self.capacity:
+      self.current = 0
+    self.storage[self.current] = item
+    self.current += 1
+    
   def get(self):
-    pass
+    new_list = []
+    for item in self.storage:
+      if item is not None:
+        new_list.append(item)
+    return new_list
+
+# test = RingBuffer(5)
+# test.append('a')
+# test.append('b')
+# test.append('c')
+# test.append('c')
+# test.append('c')
+# test.append('c')
+# # test.append('c')
+# # test.append('c')
+# # test.append('c')
+# # test.append('c')
+# # test.append('c')
+# print(test.get())
